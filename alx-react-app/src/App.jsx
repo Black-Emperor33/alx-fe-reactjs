74d97f1de3a766927055ceb959564794771f09f8
Yves Nweke <yveshenry84@gmail.com>
2025-10-31 03:50:03 +0100

    Created a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,8 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from "./components/UserProfile"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -36,6 +37,13 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+    </div>
     </>
   )
 }
