ed3fd7763ab894780bb892be5bb481bc9f6b3fcd
Yves Nweke <yveshenry84@gmail.com>
2025-11-05 05:01:13 +0100

    updated user profile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,22 @@
-const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- }
+import React, { useContext } from 'react';
+import UserContext from '../UserContext';
 
- export default UserProfile;
+function UserProfile(props) {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      {/* Props from parent (if any) */}
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+
+      {/* Data from Context */}
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
