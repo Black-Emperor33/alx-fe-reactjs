4e4a1d7aa75d439293f6791201140a046917a8c5
Yves Nweke <yveshenry84@gmail.com>
2025-11-05 05:20:37 +0100

    Built a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: '#333',
+        color: '#fff',
+        textAlign: 'center',
+        padding: '15px',
+        marginTop: '30px',
+      }}
+    >
+      <p>© Yves Nweke 2025</p>
+    </footer>
+  );
+}
+
+export default Footer;
