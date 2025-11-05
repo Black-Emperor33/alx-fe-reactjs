632627dec148a4500334743e8d879ee7dc380f95
Yves Nweke <yveshenry84@gmail.com>
2025-11-05 04:01:29 +0100

    Applied Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+     <main style={{ color: 'magneta', fontFamily: 'fantasy' , fontSize: 'bold'}}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+     </main>
+    );
+}
+
+export default MainContent;
