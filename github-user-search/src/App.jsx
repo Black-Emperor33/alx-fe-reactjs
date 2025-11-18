2360e9938b7ecea8acc4e09b9af15e70de7c268a
Yves Nweke <yveshenry84@gmail.com>
2025-11-18 03:08:49 +0100

    try and error

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <h1>GitHub User Search</h1>
+
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
