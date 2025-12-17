d0af2ea73404795e63fec8a1e17605f3d761f61d
Yves Nweke <yveshenry84@gmail.com>
2025-12-17 18:00:30 +0100

    updated profile page

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,15 +1,23 @@
-import { Outlet, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
     <div>
       <h2>Profile Page</h2>
+
       <nav>
         <Link to="/">Home</Link> |{" "}
         <Link to="details">Details</Link> |{" "}
         <Link to="settings">Settings</Link>
       </nav>
-      <Outlet />
+
+      {/* Nested routes explicitly defined */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 };
